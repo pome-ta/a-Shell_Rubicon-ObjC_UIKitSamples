2bb36f663c7bd36265f2c8e4b2d15f52b709fe98
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-03-13 12:08:25 +0900

    fix

--- exampleSFSymbolsViewer.py
+++ exampleSFSymbolsViewer.py
@@ -1,26 +1,20 @@
-import ctypes
 from pathlib import Path
 import plistlib
 
 from pyrubicon.objc.api import ObjCClass
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super
-from pyrubicon.objc.types import NSInteger
 
-from rbedge.enumerations import (
-  UITableViewStyle, )
-
+from rbedge.enumerations import UITableViewStyle
 from rbedge.functions import NSStringFromClass
-
 from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
 UITableView = ObjCClass('UITableView')
 UITableViewCell = ObjCClass('UITableViewCell')
-
-NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 UIImage = ObjCClass('UIImage')
 
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 UIColor = ObjCClass('UIColor')
 
 
@@ -36,105 +30,47 @@ def get_order_list():
 
 class SFSymbolsViewController(UIViewController):
 
-  #sfTableView: UITableView = objc_property()
   cell_identifier: str = objc_property()
   all_items: list = objc_property()
 
   @objc_method
-  def dealloc(self):
-    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    #print(f'\t - {NSStringFromClass(__class__)}: dealloc')
-    pass
-
-  @objc_method
   def loadView(self):
     send_super(__class__, self, 'loadView')
-    #print(f'\t{NSStringFromClass(__class__)}: loadView')
     self.cell_identifier = 'customCell'
     self.all_items = get_order_list()
 
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
-    #print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
 
     # --- Navigation
-    self.navigationItem.title = NSStringFromClass(__class__)
-
-    #self.view.backgroundColor = UIColor.systemDarkBlueColor()
+    self.navigationItem.title = NSStringFromClass(__class__) if (
+      title := self.navigationItem.title) is None else title
     self.view.backgroundColor = UIColor.systemBackgroundColor()
 
     # --- Table
-    sfTableView = UITableView.alloc().initWithFrame_style_(
+    sf_tableView = UITableView.alloc().initWithFrame_style_(
       self.view.bounds, UITableViewStyle.plain)
-    sfTableView.registerClass_forCellReuseIdentifier_(UITableViewCell,
-                                                      self.cell_identifier)
-
-    sfTableView.delegate = self
-    sfTableView.dataSource = self
-    #sfTableView.backgroundColor = UIColor.systemDarkRedColor()
+    sf_tableView.registerClass_forCellReuseIdentifier_(UITableViewCell,
+                                                       self.cell_identifier)
+    sf_tableView.dataSource = self
+    sf_tableView.delegate = self
 
     # --- Layout
-    self.view.addSubview_(sfTableView)
-    sfTableView.translatesAutoresizingMaskIntoConstraints = False
+    self.view.addSubview_(sf_tableView)
+    sf_tableView.translatesAutoresizingMaskIntoConstraints = False
     areaLayoutGuide = self.view.safeAreaLayoutGuide
 
     NSLayoutConstraint.activateConstraints_([
-      sfTableView.centerXAnchor.constraintEqualToAnchor_(
+      sf_tableView.centerXAnchor.constraintEqualToAnchor_(
         areaLayoutGuide.centerXAnchor),
-      sfTableView.centerYAnchor.constraintEqualToAnchor_(
+      sf_tableView.centerYAnchor.constraintEqualToAnchor_(
         areaLayoutGuide.centerYAnchor),
-      sfTableView.widthAnchor.constraintEqualToAnchor_multiplier_(
+      sf_tableView.widthAnchor.constraintEqualToAnchor_multiplier_(
         areaLayoutGuide.widthAnchor, 1.0),
-      sfTableView.heightAnchor.constraintEqualToAnchor_multiplier_(
+      sf_tableView.heightAnchor.constraintEqualToAnchor_multiplier_(
         areaLayoutGuide.heightAnchor, 1.0),
     ])
-
-  @objc_method
-  def viewWillAppear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewWillAppear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-    #print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
-
-  @objc_method
-  def viewDidAppear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewDidAppear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-    #print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
-    #print('\t↓ ---')
-
-  @objc_method
-  def viewWillDisappear_(self, animated: bool):
-    #print('\t↑ ---')
-    send_super(__class__,
-               self,
-               'viewWillDisappear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-    #print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
-
-  @objc_method
-  def viewDidDisappear_(self, animated: bool):
-    send_super(__class__,
-               self,
-               'viewDidDisappear:',
-               animated,
-               argtypes=[
-                 ctypes.c_bool,
-               ])
-    #print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
 
   @objc_method
   def didReceiveMemoryWarning(self):
@@ -143,14 +79,11 @@ class SFSymbolsViewController(UIViewController):
 
   # --- UITableViewDataSource
   @objc_method
-  def tableView_numberOfRowsInSection_(self, tableView,
-                                       section: NSInteger) -> NSInteger:
-
+  def tableView_numberOfRowsInSection_(self, tableView, section: int) -> int:
     return len(self.all_items)
 
   @objc_method
-  def tableView_cellForRowAtIndexPath_(self, tableView,
-                                       indexPath) -> ObjCClass:
+  def tableView_cellForRowAtIndexPath_(self, tableView, indexPath):
     cell = tableView.dequeueReusableCellWithIdentifier_forIndexPath_(
       self.cell_identifier, indexPath)
 
@@ -158,10 +91,10 @@ class SFSymbolsViewController(UIViewController):
 
     content = cell.defaultContentConfiguration()
     content.text = symbol_name
-    content.image = UIImage.systemImageNamed_(symbol_name)
     content.textProperties.numberOfLines = 1
-
+    content.image = UIImage.systemImageNamed_(symbol_name)
     cell.contentConfiguration = content
+
     return cell
 
   # --- UITableViewDelegate
@@ -182,9 +115,7 @@ if __name__ == '__main__':
   main_vc.navigationItem.title = _title
 
   presentation_style = UIModalPresentationStyle.fullScreen
-  #presentation_style = UIModalPresentationStyle.pageSheet
 
   app = App(main_vc, presentation_style)
   app.present()
-
 
